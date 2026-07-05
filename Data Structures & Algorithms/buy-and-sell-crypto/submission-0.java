dd0d86f22378a5d00f27ebca664bc9e11ccfdf99
Shreyash Bhosale <shreyashbhosale078@gmail.com>
2026-07-05 13:12:34 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        if(n <= 1) return 0;
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for(int i=1; i<n; i++){
+
+            if(prices[i] < minPrice){
+                minPrice = prices[i];
+            }
+            else{
+                int currProfit = prices[i] - minPrice;
+                if(currProfit > maxProfit){
+                    maxProfit = currProfit;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+}
